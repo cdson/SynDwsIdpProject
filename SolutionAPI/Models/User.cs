299c7adcc050aba6bd2c6682995429b138f35586
saket adhav <saket.adhav@synerzip.com>
2018-08-16 11:52:51 +0530

    Added users model

--- SolutionAPI/Models/User.cs
+++ SolutionAPI/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +10,35 @@ namespace SolutionAPI.Models
     {
         public int UserId { get; set; }
         public string UserName { get; set; }
+    }
+
+    public class MSGraphUser
+    {
+        [JsonProperty(PropertyName = "@odata.type")]
+        public string odataType { get; set; }
+
+        [JsonProperty(PropertyName = "@odata.id")]
+        public string odataId { get; set; }
+
+        public List<string> businessPhones { get; set; }
+        public string displayName { get; set; }
+        public string givenName { get; set; }
+        public string jobTitle { get; set; }
+        public string mail { get; set; }
+        public string mobilePhone { get; set; }
+        public string officeLocation { get; set; }
+        public string preferredLanguage { get; set; }
+        public string surname { get; set; }
+        public string userPrincipalName { get; set; }
+        public string id { get; set; }
+    }
+
+    public class MsGraphUserListResponse
+    {
+        [JsonProperty(PropertyName = "@odata.context")]
+        public string context { get; set; }
+
+        public List<MSGraphUser> value { get; set; }
     }
 
     public class MockedDataForUser
